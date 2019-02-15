a42f2cc02dc65b84231de57661caa0729cc1a91e
Slowhand0309 <slowhand0309@gmail.com>
2019-02-15 11:21:32 +0900

    add coroutines dependencies

--- app/src/main/java/com/slowhand/kt/ui/list/ListActivity.kt
+++ app/src/main/java/com/slowhand/kt/ui/list/ListActivity.kt
@@ -1,12 +1,30 @@
 package com.slowhand.kt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        main()
+    }
+
+    private fun main() = runBlocking<Unit> { // this: CoroutineScope
+        val job = launch {
+            Log.d("Activity", "CoroutineScope.launch delay 1000L before")
+            delay(1000L)
+            Log.d("Activity", "CoroutineScope.launch delay 1000L after")
+        }
+        job.join()
+        Log.d("Activity", "runBlocking delay 2000L before")
+        delay(2000L)
+        Log.d("Activity", "runBlocking delay 2000L after")
     }
 }
